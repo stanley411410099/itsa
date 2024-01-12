ab87df36e4407e23d6a2eae29c12cbecc33af991
stanley411410099 <156428178+stanley411410099@users.noreply.github.com>
2024-01-12 19:30:45 +0800

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(){
+    int x;
+    double y;
+    scanf("%d",&x);
+    y=1.6*x;
+    printf("%.1f\n",y);
+}
